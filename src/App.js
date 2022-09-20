d2cf649f85f8f9445493e06511d0878e5a830547
al-jabir <aljabir098@gmail.com>
2022-09-20 12:52:57 +0600

    I have done all components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,72 @@
-function App() {
+import { Link, Router } from "@reach/router";
+import styled from "styled-components";
+
+import Cart from "./Cart";
+import Products from "./Products";
+import { ShopProvider } from "./ShopContext";
+
+const App = () => {
   return (
-    <div className="App">
-      <h3>Test</h3>
-    </div>
+    <ShopProvider>
+      <Wrapper>
+        <TitleWrapper>
+          <h1>useReducer Hook</h1>
+          <p>
+            A <a href="https://designcode.io/">Design+Code</a> tutorial
+          </p>
+        </TitleWrapper>
+        <LinksWrapper>
+          <Link to="/">Home</Link>
+          <Link to="/cart">Cart</Link>
+        </LinksWrapper>
+        <Router>
+          <Products path="/" />
+          <Cart path="/cart" />
+        </Router>
+      </Wrapper>
+    </ShopProvider>
   );
-}
+};
 
 export default App;
+
+const Wrapper = styled.div`
+  font-family: "Roboto";
+  margin: 40px;
+
+  display: grid;
+  row-gap: 20px;
+  justify-content: center;
+`;
+
+const TitleWrapper = styled.div`
+  * {
+    margin: 0;
+  }
+
+  display: grid;
+  row-gap: 10px;
+
+  a {
+    text-decoration: none;
+    font-weight: bold;
+    color: black;
+  }
+`;
+
+const LinksWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-evenly;
+
+  a {
+    text-decoration: none;
+    color: #bb7250;
+
+    :hover {
+      color: #bb7250;
+      font-weight: bold;
+      text-decoration: underline;
+    }
+  }
+`;
